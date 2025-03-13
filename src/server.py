81da830960ed3ba4e36ea9d2f6e10ede5c989249
Padraig Alton <padraig.alton@justpark.com>
2025-03-13 10:05:17 +0000

    Modified version to improve functionality

--- src/server.py
+++ src/server.py
@@ -13,6 +13,11 @@ from src.utils import load_taskfile
 server = FastMCP("gotask", log_level="WARNING")
 
 # import tasks from Taskfile.yml
+# TODO(PaddyAlton): https://github.com/PaddyAlton/gotask-mcp/issues/1
+# how do we make this the local Taskfile, rather than the one
+# defined in this project? Presumably we'd have to manually refresh the server,
+# but it would be nice to find a way to automatically pick up the Taskfile from
+# the working project in Cursor at the point of refresh.
 all_tasks = load_taskfile()
 
 # some 'metaprogramming' here: we dynamically create a tool for each task.
@@ -43,6 +48,11 @@ for task_name, task_desc in all_tasks.items():
     # Register the task runner as a tool
     server.tool(name=task_name)(make_task_runner(task_name, task_desc))
 
+# NOTE: what if we rethink this once Cursor supports MCP resources again?
+# e.g. could we have a single tool with an argument that is the task name?
+# And a resource that runs `task --list` behind the scenes to populate
+# the argument list? And another that uses task --summary task-name
+# to get more details on a specific task?
 
 if __name__ == "__main__":
     server.run(transport="stdio")
